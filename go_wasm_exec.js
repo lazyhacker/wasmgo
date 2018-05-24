f20b36d24b747391a9a070c3eca70b00f6fb35f8
Vivek Sekhar <vivek@viveksekhar.ca>
2018-05-24 12:24:32 -0700

    Refactor to common code from app-specific code

--- go_wasm_exec.js
+++ go_wasm_exec.js
@@ -359,3 +359,26 @@
 		});
 	}
 })();
+
+if (!WebAssembly.instantiateStreaming) { // polyfill
+	WebAssembly.instantiateStreaming = async (resp, importObject) => {
+		const source = await (await resp).arrayBuffer();
+		return await WebAssembly.instantiate(source, importObject);
+	};
+}
+
+const go = new Go();
+let mod, inst;
+function load_wasm(path) {
+    WebAssembly.instantiateStreaming(fetch(path), go.importObject).then((result) => {
+        mod = result.module;
+        inst = result.instance;
+        document.getElementById("runButton").disabled = false;
+    });
+}
+
+async function run() {
+	console.clear();
+	await go.run(inst);
+	inst = await WebAssembly.instantiate(mod, go.importObject); // reset instance
+}
