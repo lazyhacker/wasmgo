12b3a48d63fbd95053fabc084601723f878218bd
Lazyhacker <github@lazyhacker.com>
2018-05-29 17:47:44 -0700

    New API calls

--- test.go
+++ test.go
@@ -11,10 +11,12 @@ import (
 
 var signal = make(chan int)
 
+// Example of a callback function that can be registered.
 func cb(args []js.Value) {
 	println("callback")
 }
 
+// cbQuit is a function that gets attached to browser event.
 func cbQuit(e js.Value) {
 	println("got Quit event callback!")
 	window := browser.GetWindow()
@@ -23,6 +25,11 @@ func cbQuit(e js.Value) {
 	signal <- 0
 }
 
+// keepalive waits for a specific value as a signal to quit.  Browsers still run
+// javascript and wasm in a single thread so until the wasm module releases
+// control, the entire browser window is blocked waiting for the module to
+// finish.  It looks like that while waiting for the blocked channel, the
+// browser window gets control back and can continue its event loop.
 func keepalive() {
 	for {
 		m := <-signal
@@ -39,13 +46,17 @@ func main() {
 
 	c := js.NewCallback(cb)
 	defer c.Close()
-	browser.Invoke(c)
-	//js.ValueOf(c).Invoke()
+	browser.Invoke(c) //js.ValueOf(c).Invoke()
 
 	window := browser.GetWindow()
 
+	// Disable the Run button so the module doesn't get executed again while it
+	// is running.  If it runs while a previous instance is still running then
+	// the browswer will give an error.
 	window.Document.GetElementById("runButton").SetAttribute("disabled", true)
 
+	// Attach a browser event to the quit button so it calls our Go code when
+	// it is clicked.  Also enable the Quit button now that the module is running.
 	window.Document.GetElementById("quit").AddEventListener(browser.EventClick, q)
 	window.Document.GetElementById("quit").SetProperty("disabled", false)
 	//js.Global.Get("document").Call("getElementById", "quit").Call("addEventListener", "click", js.ValueOf(q))
@@ -58,12 +69,14 @@ func main() {
 		window.Console.Warn(err.Error())
 	}
 
+	// Draw a cicule in the canvas.
 	canvas.Clear()
 	canvas.BeginPath()
 	canvas.Arc(100, 75, 50, 0, 2*math.Pi, false)
 	canvas.Stroke()
-	canvas.Font("30px Arial")
 
+	// A Go routine that prints its counter to the canvas.
+	canvas.Font("30px Arial")
 	time.Sleep(5 * time.Second)
 	go func() {
 		for i := 0; i < 10; i++ {
@@ -76,6 +89,7 @@ func main() {
 		canvas.FillText("Stop counting!", 10, 50)
 
 	}()
+	window.Console.Info("Go routine running while this message is printed to the console.")
 
 	keepalive()
 }
